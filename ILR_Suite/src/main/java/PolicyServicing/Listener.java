0ceeb4b67eb4f3f4ca6bc090b3bd958c6d52b3e7
NapeB <napeb@sanlamsky.co.za>
2022-05-12 09:27:14 +0200

    NewBaseClass

--- ILR_Suite/src/main/java/PolicyServicing/Listener.java
+++ ILR_Suite/src/main/java/PolicyServicing/Listener.java
@@ -1,7 +1,5 @@
 package PolicyServicing;
 
-import PolicyServicing.Base;
-import PolicyServicing.ExtentReport;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -10,7 +8,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listener extends Base implements ITestListener{
+public class Listener extends BaseNew implements ITestListener{
 
     ExtentTest test;
     ExtentReports  extent = ExtentReport.getReportObject();
